78f68883e48277f486e9ffaff0bc61ce4a92ea59
Prianka <prinkool@gmail.com>
2018-12-16 14:37:35 -0500

    create analysis.py yeet

---
+++ analysis.py
@@ -0,0 +1,7 @@
+# These scripts will form an analysis of a user's Venmo activity.
+
+# Emoji functionality
+import emoji
+
+# Takes in a list of aliases and exports emojis
+def emoji_list_producer(aliases):
