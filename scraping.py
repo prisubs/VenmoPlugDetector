797ef654f6893ffb87388ae40510be5c750c6ccf
Prianka <prinkool@gmail.com>
2018-12-16 14:00:01 -0500

    no_html_bois

--- scraping.py
+++ scraping.py
@@ -21,10 +21,24 @@ def single_site(username):
 	# Now we have all of the user's payment box objects, but need to extract descriptions
 	return payment_boxes
 
+
 # Takes in a list of transactions in HTML format and returns a list of text
-# Output format: 
+# Output format: <div class="paymentpage-text m_five_t"> TEXT HERE </div>
 def descriptions_cleaner(list):
-	return
+	# Outputted text will be just text and not HOGWASH
+	no_html_bois = []
+	for div in list:
+		# escape sequences yuhhhhh
+		DIV_HEAD = "<div class=\"paymentpage-text m_five_t\">"
+		DIV_TAIL = "</div>"
+
+		# Stripping the HTML from the page - I thought bs4 did this but apparently not
+		div = div.replace(DIV_HEAD, '')
+		div = div.replace(DIV_TAIL, '')
+
+		# Add it onto the final result
+		no_html_bois.append(div)
+	return no_html_bois
 
 
 
