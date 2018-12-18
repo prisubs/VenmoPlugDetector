8b1c67bafabeeebe1590b79e967d40a5dab58190
Prianka <prinkool@gmail.com>
2018-12-18 14:46:27 -0500

    flask good???? PleaASE??

--- driver.py
+++ driver.py
@@ -1,3 +1,4 @@
+from flask import Flask, request, render_template
 import scraping.py, analysis.py
 
 # point of contact with HTML
@@ -11,3 +12,15 @@ def generate_percentage(username):
 
 	# This result is a whole number percentage, so return a formatted string
 	return "%s's likelihood of being a dealer is %s percent.".format(username, result)
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/', methods=['POST'])
+def my_form_post():
+    user = request.form['username']
+    processed_text = generate_percentage(user)
+    return processed_text
