fe7ae1ee6f1d193e119e8b9324cee412dfa5df1d
Prianka <prinkool@gmail.com>
2018-12-18 20:58:33 -0500

    driver

--- driver.py
+++ driver.py
@@ -1,26 +1,11 @@
-from flask import Flask, request, render_template
 import scraping.py, analysis.py
-
-# point of contact with HTML
-def generate_percentage(username):
-
-	# Generate scraped output for user's name
-	cleaned_text = venmo_scraper(username)
-
-	# Pass scraped output to analysis methods
-	result = analysis(cleaned_text)
-
-	# This result is a whole number percentage, so return a formatted string
-	return "%s's likelihood of being a dealer is %s percent.".format(username, result)
+from flask import Flask
 
 app = Flask(__name__)
-
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-@app.route('/', methods=['POST'])
-def my_form_post():
-    user = request.form['username']
-    processed_text = generate_percentage(user)
-    return processed_text
+ 
+@app.route("/")
+def hello():
+	return "Hello World!"
+ 
+if __name__ == "__main__":
+	app.run()
